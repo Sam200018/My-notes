caa7003e7db707dad89d5f995d79074961a63ff0
Samuel Bautista <samuelbaubas@gmail.com>
2023-11-30 12:41:18 -0600

    Note updated, adding new attribute and complementing methods

--- app/src/main/java/mx/ipn/escom/bautistas/mynotes/domain/notes/model/Note.kt
+++ app/src/main/java/mx/ipn/escom/bautistas/mynotes/domain/notes/model/Note.kt
@@ -1,14 +1,30 @@
 package mx.ipn.escom.bautistas.mynotes.domain.notes.model
 
-import android.icu.text.CaseMap.Title
+import android.net.Uri
 import mx.ipn.escom.bautistas.mynotes.data.notes.local.NoteEntity
 
 data class Note(
     val id: Int = 0,
     val title: String = "",
-    val body: String = ""
+    val body: String = "",
+    val image: Uri? = null,
 )
 
 
-fun Note.toNoteEntity(): NoteEntity = NoteEntity(uid = id, title = title, body = body)
-fun NoteEntity.toNote(): Note = Note(id = uid, title = title, body = body)
+fun Note.toNoteEntity(): NoteEntity {
+    return if (image != null) {
+        NoteEntity(uid = id, title = title, body = body, image = image.toString())
+
+    } else
+        NoteEntity(uid = id, title = title, body = body, image = null)
+}
+
+fun NoteEntity.toNote(): Note {
+    return if (image != null) {
+
+        Note(id = uid, title = title, body = body, image = Uri.parse(image))
+    } else {
+
+        Note(id = uid, title = title, body = body, image = null)
+    }
+}
