263bee7a2f3e00e4d739f92ecc049bd93d87336b
Samuel Bautista <samuelbaubas@gmail.com>
2023-11-30 12:55:47 -0600

    home Screen updated to logout using icon button

--- app/src/main/java/mx/ipn/escom/bautistas/mynotes/ui/main/views/HomeScreen.kt
+++ app/src/main/java/mx/ipn/escom/bautistas/mynotes/ui/main/views/HomeScreen.kt
@@ -4,30 +4,41 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ExtendedFloatingActionButton
 import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Logout
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.viewmodel.compose.viewModel
 import mx.ipn.escom.bautistas.mynotes.ui.components.NotesList
 import mx.ipn.escom.bautistas.mynotes.ui.main.viewmodels.NoteViewModel
 
 @Composable
 fun HomeScreen(
     noteViewModel: NoteViewModel,
-    onNavigate: (Routes) -> Unit
+    onNavigate: (Routes) -> Unit,
+    onLogout: () -> Unit
 ) {
 
     val state by noteViewModel.state.collectAsStateWithLifecycle()
 
     Scaffold(
         modifier = Modifier.fillMaxSize(), backgroundColor = MaterialTheme.colors.background,
+        topBar = {
+            TopAppBar(title = {
+                Text(text = "Notas")
+            }, actions = {
+                IconButton(onClick = onLogout) {
+                    Icon(Icons.Filled.Logout, contentDescription = "Logout")
+                }
+            })
+        },
         floatingActionButton = {
             ExtendedFloatingActionButton(
                 text = { Text(text = "Agregar nota") },
@@ -47,8 +58,8 @@ fun HomeScreen(
     }
 }
 
-@Preview
-@Composable
-fun HomePreview() {
-    HomeScreen(viewModel()) {}
-}
+//@Preview
+//@Composable
+//fun HomePreview() {
+//    HomeScreen(hiltViewModel()) {}
+//}
