e8200dbb4188e21749f1ad45f86de8fbf4d82388
Samuel Bautista <samuelbaubas@gmail.com>
2023-11-30 12:56:58 -0600

    route component updated to add sign in screen

--- app/src/main/java/mx/ipn/escom/bautistas/mynotes/ui/main/views/Routes.kt
+++ app/src/main/java/mx/ipn/escom/bautistas/mynotes/ui/main/views/Routes.kt
@@ -2,8 +2,9 @@ package mx.ipn.escom.bautistas.mynotes.ui.main.views
 
 sealed class Routes(
     val route: String
-){
-    data object HomeScreen: Routes(route = "home")
-    data object AddNoteScreen: Routes(route = "add_note")
-    data object UpdateNoteScreen: Routes(route = "update_note")
+) {
+    data object SignInScreen : Routes(route = "Sign_in")
+    data object HomeScreen : Routes(route = "home")
+    data object AddNoteScreen : Routes(route = "add_note")
+    data object UpdateNoteScreen : Routes(route = "update_note")
 }
